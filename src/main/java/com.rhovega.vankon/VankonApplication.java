09e2afd42f94ac58a1d82da906f36b1ff30da5ef
Ashish <ashishunhale.1420@gmail.com>
2019-11-19 00:28:59 +0530

    Initial commit

---
+++ src/main/java/com.rhovega.vankon/VankonApplication.java
@@ -0,0 +1,13 @@
+package com.rhovega.vankon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VankonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VankonApplication.class, args);
+    }
+
+}
