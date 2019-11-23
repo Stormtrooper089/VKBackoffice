9ace93a2740cb5d49c8935e72d02c7fabadcb730
codesudarshan <sudarshan.shinde@ves.ac.in>
2019-11-24 01:25:05 +0530

    Models for QR Code persistsnace

---
+++ src/main/java/com.rhovega.vankon/qr/entity/QrMeta.java
@@ -0,0 +1,17 @@
+package com.rhovega.vankon.qr.entity;
+
+
+import java.util.Date;
+
+public class QrMeta {
+
+    private int id;
+    private String productId;
+    private int numberOfQrGenerated;
+    private int points;
+    private int activationStatus;
+    private Date creationDate;
+    private String createdBy;
+    private Date modifiedDate;
+    private String modifiedBy;
+}
