b7c4cc3b1f41b1abdffc26d80d106efb399fefef
Ashish <ashishunhale.1420@gmail.com>
2019-11-27 01:33:08 +0530

    Sending only pending redeem requests

--- src/main/java/com/vk/backoffice/controller/RedemptionController.java
+++ src/main/java/com/vk/backoffice/controller/RedemptionController.java
@@ -2,16 +2,24 @@ package com.vk.backoffice.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vk.backoffice.qr.entity.RVRedemptionRequest;
+import com.vk.backoffice.qr.model.CreateQrRequest;
 import com.vk.backoffice.qr.repository.RedemptionRequestRepository;
-
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
@@ -24,10 +32,18 @@ public class RedemptionController {
     public List<RVRedemptionRequest> getTransactions(){
         return getRedeemRequestListFromDatabase();
     }
+//	@PostMapping(path = "/updateRedemptionRequest")
+//    public String updateRedemptionRequest(@RequestBody RVRedemptionRequest qrRequest) {
+//        
+//        return updateRedeemRequestStatus;
+//    }
 
 	private List<RVRedemptionRequest> getRedeemRequestListFromDatabase() {
 		List<RVRedemptionRequest> redemptionlist = new ArrayList<RVRedemptionRequest>();
 		redemptionlist = redemptionRequestRepository.findAll();
-		return redemptionlist;
+		List<RVRedemptionRequest> listAfterFilters = redemptionlist.stream()
+				                                     .filter(e->e.getRedeemStatus().equalsIgnoreCase("Pending"))
+				                                     .collect(Collectors.toList());
+		return listAfterFilters;
 	}
 }
