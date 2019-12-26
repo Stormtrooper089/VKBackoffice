21cc016546e62020074216cd459c8f82438a44ea
Sudarshan Shinde <sudarshan.shinde@ves.ac.in>
2019-12-27 01:02:31 +0530

    Updating redeemption request status

--- src/main/java/com/vk/backoffice/controller/UserController.java
+++ src/main/java/com/vk/backoffice/controller/UserController.java
@@ -4,7 +4,11 @@ import com.vk.backoffice.qr.entity.User;
 import com.vk.backoffice.qr.entity.UserRole;
 import com.vk.backoffice.qr.repository.UserRepository;
 import com.vk.backoffice.qr.repository.UserRoleRepository;
+import com.vk.backoffice.qr.util.RequestStatusResponse;
+import com.vk.backoffice.qr.util.VankonConstant;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,32 +27,59 @@ public class UserController {
     @Autowired
     UserRoleRepository userRoleRepository;
 
+    Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @GetMapping("/list")
     public List<User> getUserList(){
             return userRepository.findAll();
     }
 
     @PostMapping("/createUser")
-    public void createUser(@RequestBody User user){
+    public RequestStatusResponse createUser(@RequestBody User user){
             if(user!= null){
-                User newUser = new User();
-                newUser.setDelFlg(user.getDelFlg());
-                newUser.setEmailId(user.getEmailId());
-                newUser.setMobileNumber(user.getMobileNumber());
-                newUser.setPassword(user.getPassword());
-                userRepository.save(newUser);
+                RequestStatusResponse response = new RequestStatusResponse();
+                try {
+                    User newUser = new User();
+                    newUser.setDelFlg(user.getDelFlg());
+                    newUser.setEmailId(user.getEmailId());
+                    newUser.setMobileNumber(user.getMobileNumber());
+                    newUser.setPassword(user.getPassword());
+                    userRepository.save(newUser);
+                    response.setResponseStatus(VankonConstant.SUCCESS);
+                    response.setResponseStatusDescription("User Created Successfully . .");
+                }catch (Exception e){
+                    response.setResponseStatus(VankonConstant.FAILURE);
+                    response.setResponseStatusDescription("User Creation Failed .. Try Again");
+                    logger.error(">>> CREATE USER "+e);
+                }finally {
+                    return response;
+                }
             }
+            return null;
     }
 
     @PostMapping("/updateUser")
-    public void updateUser(@RequestBody User user){
+    public RequestStatusResponse updateUser(@RequestBody User user){
         if(user!= null){
-            userRepository.save(user);
+            RequestStatusResponse response = new RequestStatusResponse();
+            try {
+                userRepository.save(user);
+                response.setResponseStatus(VankonConstant.SUCCESS);
+                response.setResponseStatusDescription("User Updated Successfully . .");
+
+            }catch (Exception e){
+                response.setResponseStatus(VankonConstant.FAILURE);
+                response.setResponseStatusDescription("User Creation Failed .. Try Again");
+                logger.error(">>> UPDATE USER "+e);
+            }finally {
+                return response;
+            }
         }
+        return null;
     }
 
     @PostMapping("/assignRole")
-    public void assignRole(@RequestBody List<UserRole> userRoleList){
+    public RequestStatusResponse assignRole(@RequestBody List<UserRole> userRoleList){
         if(userRoleList.size() > 0) {
             List<UserRole> newRoleList = new ArrayList<>();
             for(UserRole userRole: userRoleList){
@@ -58,8 +89,21 @@ public class UserController {
                 role.setUserId(userRole.getUserId());
                 newRoleList.add(role);
             }
+            RequestStatusResponse response = new RequestStatusResponse();
+            try {
             userRoleRepository.saveAll(newRoleList);
+                response.setResponseStatus(VankonConstant.SUCCESS);
+                response.setResponseStatusDescription("Roles assigned Successfully . .");
+
+            }catch (Exception e){
+                response.setResponseStatus(VankonConstant.FAILURE);
+                response.setResponseStatusDescription("Roles assignment Failed .. Try Again");
+                logger.error(">>> ROLE ASSIGNMENT "+e);
+            }finally {
+                return response;
+            }
         }
+        return null;
     }
 
     @GetMapping("/roles/{userId}")
