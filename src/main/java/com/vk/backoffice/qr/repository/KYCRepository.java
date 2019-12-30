d637dad7b055f8f737b4830c67f3c4788a396e4f
Sudarshan Shinde <sudarshan.shinde@ves.ac.in>
2019-12-31 01:32:48 +0530

    Updating redeemption request status

--- src/main/java/com/vk/backoffice/qr/repository/KYCRepository.java
+++ src/main/java/com/vk/backoffice/qr/repository/KYCRepository.java
@@ -5,8 +5,10 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.vk.backoffice.qr.entity.RVUserKyc;
+import org.springframework.data.jpa.repository.Query;
 
 
 public interface KYCRepository extends JpaRepository<RVUserKyc,Long>{
+	@Query("select e from RVUserKyc e where e.mobileNumber = :mobileNumber")
 	Optional<RVUserKyc> findByMobileNumber(String mobileNumber);
 }
